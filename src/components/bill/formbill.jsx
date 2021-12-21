75632150ee033fd30a863489c1786a81b2ef7b24
Hoangtien <letien098890098@gmail.com>
2021-12-22 00:08:37 +0700

    new

--- src/components/bill/formbill.jsx
+++ src/components/bill/formbill.jsx
@@ -2,7 +2,7 @@
 import { Button, Col, Form, Input, Row, Modal } from "antd";
 import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
-
+import "./formbill.css";
 const FormBill = (props) => {
   const showModal = () => {
     props.setIsModalVisible(true);
@@ -25,9 +25,54 @@ const FormBill = (props) => {
         onOk={handleOk}
         onCancel={handleCancel}
       >
-        <p>{props.inforProduct.product.map((item) => item.name)}</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <h1>Lamon Coffee</h1>
+        <h3>Ngày Tháng Năm : {props.inforProduct.datetime}</h3>
+        <h3>Tên Khách Hàng : {props.inforProduct.name} </h3>
+        <h3>Địa Chỉ Khách Hàng : {props.inforProduct.dc} </h3>
+        <div class="grid-container">
+          <div class="grid-item">
+            <b>Tên Món</b>
+          </div>
+          <div class="grid-item">
+            <b>Số Lượng</b>
+          </div>
+          <div class="grid-item">
+            <b>Đơn Giá</b>{" "}
+          </div>
+        </div>
+        <div class="grid-container">
+          <div class="grid-item">
+            {props.inforProduct.product?.map((item) => (
+              <p>{item.name}</p>
+            ))}
+          </div>
+          <div class="grid-item">
+            {props.inforProduct.product?.map((item) => (
+              <p>{item.qty}</p>
+            ))}
+          </div>
+          <div class="grid-item">
+            {props.inforProduct.product?.map((item) => (
+              <p>{item.gia} VND</p>
+            ))}
+          </div>
+        </div>
+        <div class="grid-container">
+          <div class="grid-item">
+            <b>Tổng Món</b>
+          </div>
+          <div class="grid-item">{props.inforProduct.CartQty}</div>
+          <div class="grid-item"> </div>
+        </div>
+        <div class="grid-container">
+          <div class="grid-item">
+            <b>Thành Tiền</b>
+          </div>
+          <div class="grid-item"></div>
+          <div class="grid-item"> {props.inforProduct.CartPrice} VND</div>
+        </div>
+        <p></p>
+        <h2>Cám Ơn Nhiều Nhe Bạn Đã Uống Nước Tại Lamon Coffee</h2>
       </Modal>
     </>
   );
